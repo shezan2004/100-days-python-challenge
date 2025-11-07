8bb3f8b0877a9bb1dccf1807825edb184f33161a
shezan2004 <shezantouhidulalam@gmail.com>
2025-11-07 08:55:57 +0600

    Day 5

---
+++ tUPPLe.py
@@ -0,0 +1,12 @@
+tup=(2,1,3,1)
+print(tup[0])
+# emplty tuple
+tup0=()
+print(tup0)
+ #single tuple
+tup1 =(1,) # comma is mandatory in single tupple
+print(tup1)
+tup2=(1,2,3,4,2,2,2,)
+# print(tup[1:3])
+print(tup2.index(2))
+print(tup2.count(2))
