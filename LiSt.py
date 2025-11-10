31a24c93e88719860b96d08bb98dd9fd100782bf
shezan2004 <shezantouhidulalam@gmail.com>
2025-11-11 04:30:24 +0600

    Day 9

---
+++ LiSt.py
@@ -0,0 +1,16 @@
+marks =[89.5, 67.9, 45.8, 90.8]
+
+print(marks)
+print(type(marks))
+print(len(marks))
+print(marks[0])
+print(marks[0:3]) #list slicing
+print(marks[-3:-1])
+
+list =[2,1,3]
+list.append(4) #2,1,3,4
+# list.sort(reverse =True)
+# print(list)
+list.insert(1,5) #2,5,1,3,4
+list.pop(2)
+print(list)
