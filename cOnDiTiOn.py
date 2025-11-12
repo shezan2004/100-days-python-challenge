78f15c4ce1c2ffe88fa1dd0dc0215c6a6ad263c7
shezan2004 <shezantouhidulalam@gmail.com>
2025-11-13 00:52:30 +0600

    Day 11

---
+++ cOnDiTiOn.py
@@ -0,0 +1,12 @@
+# age =21
+# if(age>=18):
+#     print("Can vote")
+#     print("Can vote")
+decision =input("Choose \n red \n yellow \n green \n")
+if(decision == "red"):
+    print("Stop")
+elif(decision == "green"):
+    print("Start")
+elif(decision == "yellow"):
+    print("Yellow")
+else: print("Wrong input \n Or light is brokenS")
