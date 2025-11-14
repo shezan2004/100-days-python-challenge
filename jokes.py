607f50cb2d97934937d5534a69daf602db25b347
shezan2004 <shezantouhidulalam@gmail.com>
2025-11-14 09:49:07 +0600

    Day 12

---
+++ jokes.py
@@ -0,0 +1,9 @@
+# import pyjokes
+# # print("etai bastob")
+# joke = pyjokes.get_joke()
+
+# print(joke)
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("amar sonar bangla. ami tomake vhalobasi")
+engine.runAndWait()
