b0fe6d8df9db42d322ca77ec67fe829c11dc890a
shezan2004 <shezantouhidulalam@gmail.com>
2025-11-15 21:31:04 +0600

    Day 13

---
+++ conp1.py
@@ -0,0 +1,11 @@
+mark = float(input("Enter your mark : "))
+if(mark>=90 and mark<=100):
+    print("Your Grade is : A")
+elif(mark>=80 and mark<=89):
+    print("Your Grade is : B")
+elif(mark>=70 and mark<=79):
+    print("Your Grade is : C")
+elif(mark>=60 and mark<=69):
+    print("Your Grade is : D")
+else:
+    print("Fail")
